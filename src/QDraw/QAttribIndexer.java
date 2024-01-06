ad13e03b8307bdbdad1515644c30ef4535d33001
JT <bailey.jt.brown@gmail.com>
2024-01-05 19:23:14 -0500

    WORKING ON
    > work continues...
    I just realized I screwed up big time! Damn! Should have stuck to the plan.... am gonna have to remove a bunch of stuff

--- src/QDraw/QAttribIndexer.java
+++ src/QDraw/QAttribIndexer.java
@@ -17,6 +17,83 @@ public final class QAttribIndexer {
     private int[]         attribIndexBuffer;
 
     /////////////////////////////////////////////////////////////////
+    // PUBLIC CLASSES
+    public class TriAttribs {
+        /////////////////////////////////////////////////////////////////
+        // PRIVATE MEMBERS
+        private QAttribBuffer sourceBuffer;
+        private float[] v0_dat;
+        private float[] v1_dat;
+        private float[] v2_dat;
+
+        /////////////////////////////////////////////////////////////////
+        // PUBLIC METHODS
+        public QAttribBuffer getSourceBuffer( ) { 
+            return sourceBuffer; 
+        }
+
+        public void interpolateAttribute(
+            float   weight0,
+            float   weight1,
+            float   weight2,
+            int     offsetOut,
+            float[] bufferOut
+        ) {
+            for (int comp = 0; comp < sourceBuffer.getComponentsPerAttrib(); comp++) {
+                bufferOut[offsetOut + comp] = 
+                    weight0 * v0_dat[comp] + 
+                    weight1 * v1_dat[comp] + 
+                    weight2 * v2_dat[comp];
+            }
+        }
+
+        public void getAttribute(
+            int     vertNum,
+            int     offsetOut,
+            float[] bufferOut
+        ) {
+            float[] src = null;
+            switch (vertNum) {
+                case 0:
+                    src = v0_dat;
+                    break;
+
+                case 1:
+                    src = v1_dat;
+                    break;
+                
+                case 2:
+                    src = v2_dat;
+            
+                default:
+                    throw new QException(
+                        PointOfError.BadState, 
+                        "Cannot get attribute for vertex " + vertNum + "." + 
+                        "Valid vertNums: 0, 1, 2");
+            }
+            System.arraycopy(
+                src, 
+                0, 
+                bufferOut, 
+                offsetOut, 
+                src.length
+            );
+        }
+
+        /////////////////////////////////////////////////////////////////
+        // CONSTRUCTOR
+        public TriAttribs(QAttribIndexer indexer, int triNum) {
+            sourceBuffer = indexer.attribBuffer;
+            v0_dat = new float[sourceBuffer.getComponentsPerAttrib()];
+            v1_dat = new float[sourceBuffer.getComponentsPerAttrib()];
+            v2_dat = new float[sourceBuffer.getComponentsPerAttrib()];
+            indexer.index(triNum, 0, 0, v0_dat);
+            indexer.index(triNum, 1, 0, v1_dat);
+            indexer.index(triNum, 2, 0, v2_dat);
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////
     // PUBLIC METHODS
     public int[] getIndicies( ) {
         return attribIndexBuffer;
